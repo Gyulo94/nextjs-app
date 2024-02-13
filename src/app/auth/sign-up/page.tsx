3839ec26009864e90b1dd1871296695e44a37ea4
Gyulo94 <qkrcjfrb112@gmail.com>
2024-02-13 17:40:20 +0900

    회원가입 페이지 완료 로그인페이지 만들 시간

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+const SignUpPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: { name: "", email: "", password: "" },
+  });
+  const onSubmit: SubmitHandler<FieldValues> = async (body) => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.post("/api/sign-up", body);
+      console.log(data);
+      router.push("/auth/login");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <section className="grid h-[calc(100vh_-_56px)] place-items-center">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col justify-center gap-4 min-w-[350px]"
+      >
+        <h1 className="text-2xl">Sign Up</h1>
+        <Input
+          id="email"
+          label="Email"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          id="name"
+          label="Name"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Input
+          id="password"
+          label="Password"
+          type="password"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <Button label="Register" />
+        <div className="text-center">
+          <p className="text-gray-400">
+            Already a member?{" "}
+            <Link href="/auth/sign-in" className="text-black hover:underline">
+              Sign In
+            </Link>
+          </p>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SignUpPage;
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,7 +1,11 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavItem = ({ mobile }: { mobile?: boolean }) => {
+  const { data: session, status } = useSession();
+  console.log({ session }, status);
+
   return (
     <ul
       className={`flex text-md justify-center w-full gap-4 ${
@@ -14,12 +18,15 @@ const NavItem = ({ mobile }: { mobile?: boolean }) => {
       <li className={`py-2 text-center border-b-4 cursor-pointer`}>
         <Link href={"/user"}>User</Link>
       </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Sign Out</button>
-      </li>
-      <li className={`py-2 text-center border-b-4 cursor-pointer`}>
-        <button>Sign In</button>
-      </li>
+      {session?.user ? (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </li>
+      ) : (
+        <li className={`py-2 text-center border-b-4 cursor-pointer`}>
+          <button onClick={() => signIn()}>Sign In</button>
+        </li>
+      )}
     </ul>
   );
 };
